2fdb32856bc8b33be46fb3263af68c1e4be8c365
Jason <jason@casjay.net>
2020-12-29 19:06:00 -0500

    🏠🐜❗ Minor Fixes ❗🐜🏠

--- api/endpoints/nys.js
+++ api/endpoints/nys.js
@@ -1,9 +1,17 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const datetime = require('node-datetime');
 
-const nysurl = 'https://health.data.ny.gov/resource/xdss-u53e.json';
+var dt = datetime.create();
+dt.offsetInDays(-1);
+var yesterday = dt.format('Y-m-d');
+console.log(yesterday)
+
+const nysurl = 'https://health.data.ny.gov/resource/xdss-u53e.json?test_date=' + yesterday + 'T00:00:00.000';
 const cache = null;
 const lastCacheTime = null;
+
+console.log(nysurl)
 
 async function nys() {
   if (cache && lastCacheTime > Date.now() - 1000 * 60 * 10) {
